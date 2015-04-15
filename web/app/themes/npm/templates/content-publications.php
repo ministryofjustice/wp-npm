089200d3ad767158bb26b3e8932044fbc837ee4b
Toby Schrapel <toby@tobyschrapel.com>
2015-04-15 16:02:39 +0100

    Wrap grid inside article

--- web/app/themes/npm/templates/content-publications.php
+++ web/app/themes/npm/templates/content-publications.php
@@ -1,9 +1,7 @@
-<div class="col-lg-4">
-  <article>
-    <a href="<?php the_permalink(); ?>">
-      <header>
-        <h2 class="entry-title"><?php the_title(); ?></h2>
-      </header>
-    </a>
-  </article>
-</div>
+<article <?php post_class( [ 'col-lg-4', 'col-md-6' ] ); ?>>
+  <a href="<?php the_permalink(); ?>">
+    <header>
+      <h2 class="entry-title"><?php the_title(); ?></h2>
+    </header>
+  </a>
+</article>
