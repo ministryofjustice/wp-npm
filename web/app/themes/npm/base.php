cc44ad89545893950712edcb524268b62c946fb7
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-06-01 16:18:33 +0100

    Switched sidebar to left, added featured image, sub-nav

--- web/app/themes/npm/base.php
+++ web/app/themes/npm/base.php
@@ -20,14 +20,34 @@ use Roots\Sage\Wrapper;
     ?>
     <div class="wrap container" role="document">
       <div class="content row">
-        <main class="main" role="main">
-          <?php include Wrapper\template_path(); ?>
-        </main><!-- /.main -->
         <?php if (Config\display_sidebar()) : ?>
           <aside class="sidebar" role="complementary">
+          <div class="feat-img">
+            <?php
+              $get_description = get_post(get_post_thumbnail_id())->post_excerpt;
+              the_post_thumbnail('large');
+              if(!empty($get_description)){//If description is not empty show the div
+              echo '<div class="img-caption">' . get_post(get_post_thumbnail_id())->post_excerpt . '</div>';
+            } ?>
+          </div>
+          <div class="sub-nav">
+          <?php
+            if($post->post_parent)
+            $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+            else
+            $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+            if ($children) { ?>
+              <ul>
+              <?php echo $children; ?>
+              </ul>
+          <?php } ?>
+          </div>
             <?php include Wrapper\sidebar_path(); ?>
           </aside><!-- /.sidebar -->
         <?php endif; ?>
+        <main class="main" role="main">
+          <?php include Wrapper\template_path(); ?>
+        </main><!-- /.main -->
       </div><!-- /.content -->
     </div><!-- /.wrap -->
     <?php
