e056500368b786f897a48e45a36a7d268ec74f92
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-06-03 13:43:43 +0100

    Old IE fixes

--- web/app/themes/npm/templates/head.php
+++ web/app/themes/npm/templates/head.php
@@ -6,4 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
     <?php wp_head(); ?>
+    <!--[if lt IE 9]>
+    <link rel="stylesheet" href="/npm/web/app/themes/npm/assets/styles/old-ie.css">
+    <script src="/npm/web/app/themes/npm/assets/scripts/html5shiv.js"></script>
+<![endif]-->
   </head>
