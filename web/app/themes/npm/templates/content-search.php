f8edd8b99f3574a0a1700cc48bebd3ebf1ece8b1
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2019-12-02 15:06:15 +0000

    Fix Publications Search

--- web/app/themes/npm/templates/content-search.php
+++ web/app/themes/npm/templates/content-search.php
@@ -1,6 +1,20 @@
 <article <?php post_class(); ?>>
+    <?php
+
+    $result_title = get_the_title();
+
+    if (get_post_type() === 'publications') {
+        $pdf_id = get_field('publication_file');
+        $result_url = wp_get_attachment_url($pdf_id);
+        $ext = pathinfo($result_url, PATHINFO_EXTENSION);
+        $result_title .= ' (' . $ext . ')';
+    }
+    else {
+        $result_url = get_permalink();
+    }
+    ?>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2 class="entry-title"><a href="<?php echo $result_url; ?>"><?php echo $result_title; ?></a></h2>
     <?php if (get_post_type() === 'post') { get_template_part('templates/entry-meta'); } ?>
   </header>
   <div class="entry-summary">
